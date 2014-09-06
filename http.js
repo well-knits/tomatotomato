b06c2272f2ebb4be2e9f7e48bf0dd107dc08a670
David Björklund <david.bjorklund@gmail.com>
2014-09-06 21:03:53 +0200

    cleanup

--- http.js
+++ http.js
@@ -1,40 +1,41 @@
 #!/usr/bin/env node
 
-var format = require('./format')
+var fs = require('fs')
+  , http = require('http')
+
+  , serveBrowserify = require('serve-browserify')({
+      root: __dirname + '/public'
+    })
+  , ws = require('ws')
 
   , connection = require('./connect')()
+  , format = require('./format')
 
-  , WebSocketServer = require('ws').Server
-  , serveBrowserify = require('serve-browserify')({
-       root: __dirname + '/public'
-   })
-  , server = require('http').createServer(function (req, res) {
+  , server = http.createServer(function (req, res) {
       if (req.url === '/client.js') {
         serveBrowserify(req, res)
       } else {
-        require('fs').readFile(__dirname + '/public/index.html', function (err, html) {
-
+        fs.readFile(__dirname + '/public/index.html', function (err, html) {
 
           res.writeHead(200, { 'content-type': 'text/html' })
           res.end(html)
         })
       }
-   }).listen(2929)
+    }).listen(2929)
 
-  , wss = new WebSocketServer({ server: server })
+ws.createServer({ server: server }, function (socket) {
+  var send = function (data) { socket.send(JSON.stringify(data)) }
 
-wss.on('connection', function(ws) {
   var ondata = function (obj) {
-        var data = { countdown: format(obj.countdown), type: obj.type }
-        ws.send(JSON.stringify(data))
-      },
-      onclose = function () {
-        ws.send(JSON.stringify({ type: 'close' }));
+        send({ countdown: format(obj.countdown), type: obj.type })
+      }
+    , onclose = function () {
+        send({ type: 'close' })
       }
 
   connection.on('data', ondata)
   connection.on('close', onclose)
-  ws.on('close', function () {
+  socket.on('close', function () {
     connection.removeListener('data', ondata)
     connection.removeListener('close', onclose)
   })
